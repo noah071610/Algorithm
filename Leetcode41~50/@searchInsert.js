2a741c3c285dc666549d29824444a13cb7e2b4cf
noah <noah071610@naver.com>
2021-10-25 17:47:15 +0900

    릿코드 1, css animation 1

---
+++ Leetcode41~50/@searchInsert.js
@@ -0,0 +1,6 @@
+var searchInsert = function (nums, target) {
+  return nums.findIndex((v) => v >= target) === -1 ? nums.length : answer;
+};
+let num = [1, 2, 3, 0, 0, 0];
+console.log(searchInsert());
+console.log(num);
