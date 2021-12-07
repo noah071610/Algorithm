636a6b6d3238a88fb36da8888604adf0942e299f
Noah Jang <noah071610@gmail.com>
2021-12-07 22:10:19 +0900

    valid anagram

---
+++ Leetcode41~50/validAnagram.js
@@ -0,0 +1,8 @@
+var isAnagram = function(s, t) {
+	const map = {};
+	s.split('').map(c => map[c] = map[c] ? map[c] + 1 : 1);
+	t.split('').map(c => map[c] = map[c] ? map[c] - 1 : -1);
+	return Object.keys(map).every(k => map[k] === 0);
+};
+
+console.log(isAnagram('anagram','nagaram'));
